ce6c46815b3d053b43f5be566c3ab67ce5907e0a
Paul Irish <paul.irish@gmail.com>
2014-12-15 22:06:38 -0800

    resolving non-JSON expressions to valid JSON. wdyt, @dgozman?

--- network-conditions.js
+++ network-conditions.js
@@ -4,8 +4,8 @@
         { "id": "gprs", "title" : "GPRS", "throughput": 50, "latency": 500},
         { "id": "edge", "title" : "EDGE", "throughput": 250, "latency": 300},
         { "id": "3g", "title" : "3G", "throughput": 750, "latency": 100},
-        { "id": "dsl", "title" : "DSL", "throughput": 2 * 1024, "latency": 5},
-        { "id": "wifi", "title" : "WiFi", "throughput": 30 * 1024, "latency": 2},
-        { "id": "online", "title" : "No throttling", "throughput": WebInspector.OverridesSupport.NetworkThroughputUnlimitedValue, "latency": 0}
+        { "id": "dsl", "title" : "DSL", "throughput": 2048, "latency": 5},
+        { "id": "wifi", "title" : "WiFi", "throughput": 30720, "latency": 2},
+        { "id": "online", "title" : "No throttling", "throughput": -1, "latency": 0}
     ]
 }
